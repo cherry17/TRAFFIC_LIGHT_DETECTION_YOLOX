ecd4d626cd2367f3d3a91804750c49ec1af7b0b9
CHERRY AGRAWAL <cherryagrawal17@gmail.com>
2022-08-06 23:46:30 +0530

    Add files via upload

---
+++ yolox/data/datasets/coco_classes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+COCO_CLASSES = (
+    "RedLeft",
+    "Red",    
+    "RedRight",   
+    "GreenLeft",    
+    "Green",
+    "GreenRight",    
+    "Yellow",    
+    "off",
+    "RedStraight",
+    'RedStraightLeft',
+    'RedStraightRight',
+    "GreenStraight",
+    'GreenStraightRight',
+    'GreenStraightLeft'
+)
---
+++ yolox/data/datasets/voc_classes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+# VOC_CLASSES = ( '__background__', # always index 0
+VOC_CLASSES = (
+    "RedLeft",
+    "Red",    
+    "RedRight",   
+    "GreenLeft",    
+    "Green",
+    "GreenRight",    
+    "Yellow",    
+    "off",
+    "RedStraight",
+    'RedStraightLeft',
+    'RedStraightRight',
+    "GreenStraight",
+    'GreenStraightRight',
+    'GreenStraightLeft'
+)
